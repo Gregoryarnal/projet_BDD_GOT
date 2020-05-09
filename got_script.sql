1d28fda72b57e1b683e8c3706b0798cd04204a98
Gregory <gregory.arnal@icloud.com>
2020-05-10 01:03:09 +0200

    [ADD] add famille and fix some syntaxe bug

--- got_script.sql
+++ got_script.sql
@@ -3,85 +3,83 @@
 
 -- Modify this code to update the DB schema diagram.
 -- To reset the sample schema, replace everything with
--- two dots ('..' - without quotes).
+-- two dots (.. - without quotes).
 
-CREATE TABLE `Personnage` (
-    `id_personnage` int  NOT NULL ,
-    `Name` string  NOT NULL ,
-    `Surname` string  NOT NULL ,
-    `tué_par` int  NOT NULL ,
-    `famille` int  NOT NULL ,
-    `civilisation` int  NOT NULL ,
+CREATE TABLE Personnage (
+    id_personnage int  NOT NULL AUTO_INCREMENT,
+    Name varchar2(50)  NOT NULL ,
+    Surname varchar2(50)  NOT NULL ,
+    tué_par int ,
+    famille int ,
+    civilisation int,
     PRIMARY KEY (
-        `id_personnage`
+        id_personnage
     )
 );
 
-CREATE TABLE `Chateau` (
-    `id_chateau` int  NOT NULL ,
-    `lieu` string  NOT NULL ,
-    `roi` int  NOT NULL ,
-    `famille` int  NOT NULL ,
+CREATE TABLE Chateau (
+    id_chateau int  NOT NULL AUTO_INCREMENT,
+    lieu varchar2(50)  NOT NULL ,
+    roi int  NOT NULL ,
+    famille int  NOT NULL ,
     PRIMARY KEY (
-        `id_chateau`
+        id_chateau
     )
 );
 
-CREATE TABLE `Famille` (
-    `id_famille` int  NOT NULL ,
-    `nom` string  NOT NULL ,
-    `histoire` string  NOT NULL ,
+CREATE TABLE Famille (
+    id_famille int  NOT NULL AUTO_INCREMENT,
+    nom varchar(50)  NOT NULL UNIQUE,
+    histoire varchar(50),
+    CONSTRAINT pk_id PRIMARY KEY (id_famille)
+);
+
+CREATE TABLE Enfant_de (
+    id int  NOT NULL ,
+    id_pere int  NOT NULL ,
+    id_mere int  NOT NULL 
+);
+
+CREATE TABLE A_couche (
+    id1 int  NOT NULL ,
+    id2 int  NOT NULL 
+);
+
+CREATE TABLE A_tué (
+    tueur int  NOT NULL ,
+    tuées int  NOT NULL 
+);
+
+CREATE TABLE Guerre (
+    id int  NOT NULL AUTO_INCREMENT,
+    nom varchar2(50)  NOT NULL ,
+    lieu id  NOT NULL ,
+    dirigeant id  NOT NULL ,
     PRIMARY KEY (
-        `id_famille`
+        id
     )
 );
 
-CREATE TABLE `Enfant_de` (
-    `id` int  NOT NULL ,
-    `id_pere` int  NOT NULL ,
-    `id_mere` int  NOT NULL 
+CREATE TABLE Marie_a (
+    époux int  NOT NULL ,
+    épouse int  NOT NULL 
 );
 
-CREATE TABLE `A_couche` (
-    `id1` int  NOT NULL ,
-    `id2` int  NOT NULL 
-);
-
-CREATE TABLE `A_tué` (
-    `tueur` int  NOT NULL ,
-    `tuées` int  NOT NULL 
-);
-
-CREATE TABLE `Guerre` (
-    `id` int  NOT NULL ,
-    `nom` string  NOT NULL ,
-    `lieu` id  NOT NULL ,
-    `dirigeant` id  NOT NULL ,
+CREATE TABLE Lieu (
+    id_lieu int  NOT NULL AUTO_INCREMENT,
+    nom varchar2(50)  NOT NULL ,
+    sous_le_controle_de int  NOT NULL ,
     PRIMARY KEY (
-        `id`
+        id_lieu
     )
 );
 
-CREATE TABLE `Marie_a` (
-    `époux` int  NOT NULL ,
-    `épouse` int  NOT NULL 
-);
-
-CREATE TABLE `Lieu` (
-    `id_lieu` int  NOT NULL ,
-    `nom` string  NOT NULL ,
-    `sous_le_controle_de` int  NOT NULL ,
+CREATE TABLE Armee (
+    id int  NOT NULL AUTO_INCREMENT,
+    nom varchar2(50)  NOT NULL ,
+    chef int  NOT NULL ,
     PRIMARY KEY (
-        `id_lieu`
-    )
-);
-
-CREATE TABLE `Armee` (
-    `id` int  NOT NULL ,
-    `nom` string  NOT NULL ,
-    `chef` int  NOT NULL ,
-    PRIMARY KEY (
-        `id`
+        id
     )
 );
 
@@ -89,62 +87,67 @@ CREATE TABLE `Armee` (
 
 
 
-ALTER TABLE `Personnage` ADD CONSTRAINT `fk_Personnage_famille` FOREIGN KEY(`famille`)
-REFERENCES `Famille` (`id_famille`);
+ALTER TABLE Personnage ADD CONSTRAINT fk_Personnage_famille FOREIGN KEY(famille)
+REFERENCES Famille (id_famille);
 
-ALTER TABLE `Personnage` ADD CONSTRAINT `fk_Personnage_civilisation` FOREIGN KEY(`civilisation`)
-REFERENCES `Table 11` (`...`);
+ALTER TABLE Chateau ADD CONSTRAINT fk_Chateau_roi FOREIGN KEY(roi)
+REFERENCES Personnage (id_personnage);
 
-ALTER TABLE `Chateau` ADD CONSTRAINT `fk_Chateau_roi` FOREIGN KEY(`roi`)
-REFERENCES `Personnage` (`id_personnage`);
+ALTER TABLE Chateau ADD CONSTRAINT fk_Chateau_famille FOREIGN KEY(famille)
+REFERENCES Famille (id_famille);
 
-ALTER TABLE `Chateau` ADD CONSTRAINT `fk_Chateau_famille` FOREIGN KEY(`famille`)
-REFERENCES `Famille` (`id_famille`);
+ALTER TABLE Enfant_de ADD CONSTRAINT fk_Enfant_de_id FOREIGN KEY(id)
+REFERENCES Personnage (id_personnage);
 
-ALTER TABLE `Enfant_de` ADD CONSTRAINT `fk_Enfant_de_id` FOREIGN KEY(`id`)
-REFERENCES `Personnage` (`id_personnage`);
+ALTER TABLE Enfant_de ADD CONSTRAINT fk_Enfant_de_id_pere FOREIGN KEY(id_pere)
+REFERENCES Personnage (id_personnage);
 
-ALTER TABLE `Enfant_de` ADD CONSTRAINT `fk_Enfant_de_id_pere` FOREIGN KEY(`id_pere`)
-REFERENCES `Personnage` (`id_personnage`);
+ALTER TABLE Enfant_de ADD CONSTRAINT fk_Enfant_de_id_mere FOREIGN KEY(id_mere)
+REFERENCES Personnage (id_personnage);
 
-ALTER TABLE `Enfant_de` ADD CONSTRAINT `fk_Enfant_de_id_mere` FOREIGN KEY(`id_mere`)
-REFERENCES `Personnage` (`id_personnage`);
+ALTER TABLE A_couche ADD CONSTRAINT fk_A_couche_id1 FOREIGN KEY(id1)
+REFERENCES Personnage (id_personnage);
 
-ALTER TABLE `A_couche` ADD CONSTRAINT `fk_A_couche_id1` FOREIGN KEY(`id1`)
-REFERENCES `Personnage` (`id_personnage`);
+ALTER TABLE A_couche ADD CONSTRAINT fk_A_couche_id2 FOREIGN KEY(id2)
+REFERENCES Personnage (id_personnage);
 
-ALTER TABLE `A_couche` ADD CONSTRAINT `fk_A_couche_id2` FOREIGN KEY(`id2`)
-REFERENCES `Personnage` (`id_personnage`);
+ALTER TABLE A_tué ADD CONSTRAINT fk_A_tué_tueur FOREIGN KEY(tueur)
+REFERENCES Personnage (id_personnage);
 
-ALTER TABLE `A_tué` ADD CONSTRAINT `fk_A_tué_tueur` FOREIGN KEY(`tueur`)
-REFERENCES `Personnage` (`id_personnage`);
+ALTER TABLE A_tué ADD CONSTRAINT fk_A_tué_tuées FOREIGN KEY(tuées)
+REFERENCES Personnage (id_personnage);
 
-ALTER TABLE `A_tué` ADD CONSTRAINT `fk_A_tué_tuées` FOREIGN KEY(`tuées`)
-REFERENCES `Personnage` (`id_personnage`);
+ALTER TABLE Guerre ADD CONSTRAINT fk_Guerre_lieu FOREIGN KEY(lieu)
+REFERENCES Lieu (id_lieu);
 
-ALTER TABLE `Guerre` ADD CONSTRAINT `fk_Guerre_lieu` FOREIGN KEY(`lieu`)
-REFERENCES `Lieu` (`id_lieu`);
+ALTER TABLE Guerre ADD CONSTRAINT fk_Guerre_dirigeant FOREIGN KEY(dirigeant)
+REFERENCES Personnage (id_personnage);
 
-ALTER TABLE `Guerre` ADD CONSTRAINT `fk_Guerre_dirigeant` FOREIGN KEY(`dirigeant`)
-REFERENCES `Personnage` (`id_personnage`);
+ALTER TABLE Marie_a ADD CONSTRAINT fk_Marie_a_époux FOREIGN KEY(époux)
+REFERENCES Personnage (id_personnage);
 
-ALTER TABLE `Marie_a` ADD CONSTRAINT `fk_Marie_a_époux` FOREIGN KEY(`époux`)
-REFERENCES `Personnage` (`id_personnage`);
+ALTER TABLE Marie_a ADD CONSTRAINT fk_Marie_a_épouse FOREIGN KEY(épouse)
+REFERENCES Personnage (id_personnage);
 
-ALTER TABLE `Marie_a` ADD CONSTRAINT `fk_Marie_a_épouse` FOREIGN KEY(`épouse`)
-REFERENCES `Personnage` (`id_personnage`);
+ALTER TABLE Lieu ADD CONSTRAINT fk_Lieu_sous_le_controle_de FOREIGN KEY(sous_le_controle_de)
+REFERENCES Personnage (id_personnage);
 
-ALTER TABLE `Lieu` ADD CONSTRAINT `fk_Lieu_sous_le_controle_de` FOREIGN KEY(`sous_le_controle_de`)
-REFERENCES `Personnage` (`id_personnage`);
-
-ALTER TABLE `Armee` ADD CONSTRAINT `fk_Armee_chef` FOREIGN KEY(`chef`)
-REFERENCES `Personnage` (`id_personnage`);
-
--- Free plan table limit reached. SUBSCRIBE for more.
+ALTER TABLE Armee ADD CONSTRAINT fk_Armee_chef FOREIGN KEY(chef)
+REFERENCES Personnage (id_personnage);
 
 
+-- Famille DATA
+-----------------------------------------------------
+INSERT INTO Famille (nom) values ("Stark");
+INSERT INTO Famille (nom) values ("Lannister");
+INSERT INTO Famille (nom) values ("Baratheon");
+INSERT INTO Famille (nom) values ("Tyrell");
+INSERT INTO Famille (nom) values ("Martell");
+INSERT INTO Famille (nom) values ("Tully");
+INSERT INTO Famille (nom) values ("Arryn");
+INSERT INTO Famille (nom) values ("Greyjoy");
+INSERT INTO Famille (nom) values ("Bolton");
+INSERT INTO Famille (nom) values ("Clegane");
+INSERT INTO Famille (nom) values ("Targaryen");
 
--- Free plan table limit reached. SUBSCRIBE for more.
-
-
-
+ 
